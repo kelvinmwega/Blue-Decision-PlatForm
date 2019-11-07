3303cb8bbb85ff6f4fc0fe1f81f72d59272a3861
Kelvin <kelvin@hellotractor.com>
2019-11-08 02:13:20 +0300

    api calls waterpoint api and weather api

---
+++ API/src/service/genericRepository/genericCtrl.ts
@@ -0,0 +1,27 @@
+import * as request from "request-promise-native";
+
+export class GenericController {
+
+    private headersOpt = {
+        "content-type": "application/json",
+    };
+
+    public allSensors = async () => {
+        var options = {
+            uri: "https://waterpoint-engine-challenge-dev.mybluemix.net/sensors",
+            headers: this.headersOpt
+        };
+
+        return await request.get(options);
+    }
+
+    public weatherForecast = async (lat, lon) => {
+        var options = {
+            uri: `https://api.darksky.net/forecast/1e7ede044c6d02abd140215378af0959/${lat},${lon}?units=si`,
+            headers: this.headersOpt
+        };
+
+        return await request.get(options);
+    }
+
+}
--- API/src/service/genericRepository/route.ts
+++ API/src/service/genericRepository/route.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express";
 import logger from '../../utils/logger';
 import { stringify } from "querystring";
+import { GenericController } from "./genericCtrl";
 
 export default [
   {
     path: "/api/v1/:repository",
     method: "get",
     handler: async (req: Request, res: Response) => {
-      logger.info(`parameter ${req.params.repository}`);
-      res.send(req.params.repository);
+      logger.info(`collecting sensors `);
+
+      const generic = new GenericController();
+      let sensors = await generic.allSensors();
+      sensors = JSON.parse(sensors.trim());
+
+      logger.info(`complete ${sensors.statusCode}`);
+      res.send(sensors);
+    }
+  },
+  {
+    path: "/api/weather",
+    method: "get",
+    handler: async (req: Request, res: Response) => {
+      logger.info(`collecting weather ${JSON.stringify(req.body)}`);
+
+      const generic = new GenericController();
+      let forecast = await generic.weatherForecast(req.body.lat, req.body.lon);
+      forecast = JSON.parse(forecast.trim());
+
+      logger.info(`complete current weather ${forecast.currently.summary}`);
+      res.send(forecast);
     }
   },
 
