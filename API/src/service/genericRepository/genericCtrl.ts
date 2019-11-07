1475ecfa2147ad9a11416ddddd4c8576f7b7ece0
Kelvin <kelvin@hellotractor.com>
2019-11-08 02:44:52 +0300

    url modification

--- API/src/service/genericRepository/genericCtrl.ts
+++ API/src/service/genericRepository/genericCtrl.ts
@@ -17,7 +17,7 @@ export class GenericController {
 
     public weatherForecast = async (lat, lon) => {
         var options = {
-            uri: `https://api.darksky.net/forecast/1e7ede044c6d02abd140215378af0959/${lat},${lon}?units=si`,
+            uri: `https://api.darksky.net/forecast/1e7ede044c6d02abd140215378af0959/${lat},${lon}?exclude=hourly,flags&units=si`,
             headers: this.headersOpt
         };
 
